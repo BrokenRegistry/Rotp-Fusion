1aa891ada89472f4df43e75f024f108b345da094
BrokenRegistry <98669686+BrokenRegistry@users.noreply.github.com>
2025-09-29 23:06:10 -0400

    Try to fix Pom not working anymore

--- src/rotp/PomUpdater.java
+++ src/rotp/PomUpdater.java
@@ -1,6 +1,14 @@
 package rotp;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -8,8 +16,9 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class PomUpdater {
-	private static final String VER_B = "<version>";
-	private static final String VER_E = "</version>";
+	private static final String VERSION_SEARCH	= "<version>Fusion";
+	private static final String VERSION_BEGIN	= "<version>";
+	private static final String VERSION_END		= "</version>";
 	static void updatePom()	{
 		String folder = Rotp.jarPath() + "/..";
 		String name = "pom.xml";
@@ -32,47 +41,47 @@ public class PomUpdater {
 		String currentVersion = "";
 		ArrayList<String> content = new ArrayList<>();
 
-//		// Load and modify if needed
-//		try ( BufferedReader in = new BufferedReader( new InputStreamReader( new FileInputStream(pomFile), "UTF-8"));) {
-//			String input;
-//			while (!sameVersion && (input = in.readLine()) != null) {
-//				if (input.contains(VER_B)) {
-//					int start = input.indexOf(VER_B) + VER_B.length();
-//					int end = input.indexOf(VER_E);
-//					currentVersion = input.substring(start, end);
-//					sameVersion = newVersion.equals(currentVersion);
-//					if (sameVersion)
-//						return false;
-//					String output = input.replace(currentVersion, newVersion);
-//					content.add(output);
-//					tobeUpdated = true;
-//				}
-//				else
-//					content.add(input);
-//			}
-//		}
-//		catch (FileNotFoundException e) {
-//			System.err.println(pomFile.toString() + " not found.");
-//		}
-//		catch (IOException e) {
-//			System.err.println("PomFile update load -- IOException: "+ e.toString());
-//		}
-//
-//		if (!tobeUpdated) {
-//			System.err.println("No version line found in the PomFile");
-//			return false;
-//		}
-//
-//		// Write if modified
-//		try (FileOutputStream fout = new FileOutputStream(pomFile);
-//			PrintWriter out = new PrintWriter(new OutputStreamWriter(fout, "UTF-8")); ) {
-//				for (String line : content)
-//					out.println(line);
-//			}
-//			catch (IOException e) {
-//				System.err.println("PomFile update save -- IOException: "+ e.toString());
-//				return false;
-//			}
+		// Load and modify if needed
+		try ( BufferedReader in = new BufferedReader( new InputStreamReader( new FileInputStream(pomFile), "UTF-8"));) {
+			String input;
+			while (!sameVersion && (input = in.readLine()) != null) {
+				if (input.contains(VERSION_SEARCH)) {
+					int start = input.indexOf(VERSION_BEGIN) + VERSION_BEGIN.length();
+					int end = input.indexOf(VERSION_END);
+					currentVersion = input.substring(start, end);
+					sameVersion = newVersion.equals(currentVersion);
+					if (sameVersion)
+						return false;
+					String output = input.replace(currentVersion, newVersion);
+					content.add(output);
+					tobeUpdated = true;
+				}
+				else
+					content.add(input);
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.err.println(pomFile.toString() + " not found.");
+		}
+		catch (IOException e) {
+			System.err.println("PomFile update load -- IOException: "+ e.toString());
+		}
+
+		if (!tobeUpdated) {
+			System.err.println("No version line found in the PomFile");
+			return false;
+		}
+
+		// Write if modified
+		try (FileOutputStream fout = new FileOutputStream(pomFile);
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(fout, "UTF-8")); ) {
+				for (String line : content)
+					out.println(line);
+			}
+			catch (IOException e) {
+				System.err.println("PomFile update save -- IOException: "+ e.toString());
+				return false;
+			}
 		return true;
 	}
 }
